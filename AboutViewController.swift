22a4302a4fd1bd4b69f0765b3d725a8b421c77dc
onurTe <onur.tetik@gmail.com>
2019-02-06 12:01:32 +0300

    first iOs app

---
+++ AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by onur  on 18.01.2019.
+//  Copyright © 2019 onur . All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+}
